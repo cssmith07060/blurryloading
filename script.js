53d8fc18cb25e4d1b945348bc7787968ceea6e24
cssmith07060 <cssmith07060@gmail.com>
2022-05-11 16:56:35 -0400

    king

--- script.js
+++ script.js
@@ -10,5 +10,12 @@ function blurring() {
     if (load > 99) {
         clearInterval(int)
     }
+    
     loadText.innerText = `${load}%`
+    loadText.getElementsByClassName.opacity = scale(load, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
